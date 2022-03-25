db443d058b88e7290a7cf3c2f0df53875a9064af
Laxmi <laxmik298@gmail.com>
2022-03-25 19:19:02 +0530

    2 form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-form';
+  loginForm= new FormGroup({
+    'userDetail': new FormGroup({
+      username:new FormControl(null,Validators.required),
+      password:new FormControl(null,Validators.required)
+    })
+  })
+  // constructor(){
+  // }
+  // --------start Teamplate Driven -------------
+  // onSubmit(data: any){
+  //   console.log("data")
+  //   console.log(data)
+  // }
+  // --------end template driven --------------
+
+  onSubmit(){
+    console.table("data",this.loginForm.value)
+  }
+  
 }
